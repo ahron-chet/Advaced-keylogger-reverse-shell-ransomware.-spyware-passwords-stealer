c621254158e104321957ffdadd7d5e8e237fd273
ahron-chet <105120555+ahron-chet@users.noreply.github.com>
2022-06-22 16:46:47 +0300

    Update scanner_port.py

--- scanner_port.py
+++ scanner_port.py
@@ -1,19 +1,24 @@
+###################################
+##   Port scanner,               ##
+##   vulnerabilities scanner.    ## 
+###################################
+
 import socket
 from socket import getservbyname, getservbyport
 
 class Scanner_ports():
+    
     def __init__(self,host,port):
         self.host=host
         self.port=port
         self.baners_verision=[]
         self.counter=0
-        self.out_data=''
            
         
     def port_scaner(self,host,port):
         try:
             sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.settimeout(0.5)
+            sock.settimeout(0.0001)
             sock.connect((host,port))
             try:
                 service_baner = sock.recv(1024).decode()
@@ -25,7 +30,6 @@ class Scanner_ports():
             except:
                 port_name = ''
             print('{+}'+' port '+port_name+' '+str(port)+" is open! "+service_baner[:-1])
-            self.out_data+='{+}'+' port '+port_name+' '+str(port)+" is open! "+service_baner[:-1]+'\n'
             self.counter=1
 
         except socket.error:
@@ -41,14 +45,14 @@ class Scanner_ports():
         strat_range_port=int(strat_range_port)
         end_range_port=int(end_range_port)
         host=self.get_host_name(host)
-        self.out_data+=('\nscannig for open ports on '+host+'...\n')
-        self.out_data+=('='*50)+'\n'
+        print('\nscannig for open ports on '+host+'...')
+        print('='*50)
         for i in range(strat_range_port,end_range_port+1):
             self.port_scaner(host,i)
         if self.counter==0:
-            self.out_data+=("not found..")+'\n'
+            print("not found..")
         self.counter=0
-        self.out_data+=('-'*50)+'\n'
+        print('-'*50)
             
 
 
@@ -92,7 +96,7 @@ class Scanner_ports():
             ranges=self.ip_range(start_ip,end_ip)
             for i in ranges:
                  self.range_of_ports(start_port,end_port,i)
-            self.out_data+=('end.')+'\n'
+            print('end.')
     
         else:
             if '-'in self.port:
@@ -105,8 +109,7 @@ class Scanner_ports():
                 
             host=self.get_host_name(self.host)
             self.range_of_ports(start_port,end_port,host)
-            self.out_data+=('end.')+'\n'
-        return self.out_data
+            print('end.')
             
             
             
@@ -119,28 +122,27 @@ class Scanner_ports():
         
         self.scaning_for_ports_open()
         p=len(self.baners_verision)-1
-        self.out_data+=('\n\nvulnerabilities')+'\n'
-        self.out_data+=('='*50)+'\n'
+        print('\n\nvulnerabilities')
+        print('='*50)
         count=0
 
         for i in self.baners_verision:
             v=str(i).split(' on host')
             v=v[0]
             if v in vulnerabilities:
-                self.out_data+=(i)+'\n'
+                print(i)
                 count=1
         if count==0:
-            self.out_data+=('not found vulnerabilities')
-        return self.out_data
+            print('not found vulnerabilities')
 
 def main():
-    v=str(input("to scan for ports enter 1 for ports and vulnerabilities enter full path of vulnerabilitie list: "))
-    p=input('\n\nTo scan for one target and one port enter ip, port (ex:192.167.1.1, 80)\nto scan range of ports enter ip, start_port-end_port (ex: 192.167.1.1, 0-500)\nto scan multiple targets enter start_ip-end_ip ,port/start_port-end_port (ex:10.0.0.1-10.0.0.6, 0-500:\n')
+    v=str(input("to scan for ports enter 1.\nto scan ports and vulnerabilities, enter full path of vulnerabilitie list:\n"))
+    p=input('\nTo scan for one target and one port enter: <ip>, <port> (ex:192.167.1.1, 80).\nto scan range of ports enter <ip>, <start port-end port> (ex: 192.167.1.1, 0-500).\nto scan multiple targets enter <start ip-end ip> ,<port>/<start port>-<end port> (ex:10.0.0.1-10.0.0.6, 0-500:\n')
     p=p.split(',')
     if v=='1':
-        print(Scanner_ports(p[0],p[-1]).scaning_for_ports_open())
+        Scanner_ports(p[0],p[-1]).scaning_for_ports_open()
     else:
-        print(Scanner_ports(p[0],p[-1]).scan_for_vulnerabilities(v))
+        Scanner_ports(p[0],p[-1]).scan_for_vulnerabilities(v)
 
 
 if __name__=='__main__':
